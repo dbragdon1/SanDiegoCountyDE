f5d75da98205202b35493ff34c84a924f6a45f1d
Dylan Bragdon <dylan.bragdon@gmail.com>
2021-04-30 15:27:01 -0700

    added vax rate map

---
+++ create_vax_rate_map.py
@@ -0,0 +1,24 @@
+import folium
+import requests
+import json
+import geopandas as gpd
+
+url = "https://opendata.arcgis.com/datasets/da83fdaab14e42f0b3fe198a15c5bad5_0.geojson"
+
+print('Loading geojson file...')
+geo_obj = json.loads(requests.get(url).content)
+
+print('Reading geojson file to geopandas dataframe...')
+gdf = gpd.read_file(url)
+
+print('Creating map...')
+m = folium.Map(location = [32.75, -117.10])
+c = folium.features.Choropleth(geo_data = url,
+				data = gdf,
+				columns = ['zip', 'individuals_vaccinated_with_at_'],
+				key_on = 'feature.properties.zip')
+c.add_to(m)
+
+m.save('vax.html')
+
+print('Finished')
